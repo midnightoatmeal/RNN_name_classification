11e7e6faeb005de3a5ec796c94cfb9726c91dd54
Lionel Rozario <lionelrozario98@gmail.com>
2025-09-26 18:06:41 -0400

    defined read_lines and letter_to_index and letter_to_tensor

--- utils.py
+++ utils.py
@@ -27,4 +27,41 @@ def load_data():
     def find_files(path):
         return glob.glob(path)
     
-    
+    # Read a file and split into lines
+    def read_lines(filename):
+        lines = io.open(filename, encoding='utf-8').read().strip().split('\n')
+        return [unicode_to_ascii(line) for line in lines]
+    for filename in find_files('data/names/*.txt'):
+        category = os.path.splitext(os.path.basename(filename))[0]
+        all_categories.append(category)
+
+        lines = read_lines(filename)
+        category_lines[category] = lines
+
+    return category_lines, all_categories
+
+"""
+To represent a single letter, we use a “one-hot vector” of 
+size <1 x n_letters>. A one-hot vector is filled with 0s
+except for a 1 at index of the current letter, e.g. "b" = <0 1 0 0 0 ...>.
+
+To make a word we join a bunch of those into a
+2D matrix <line_length x 1 x n_letters>.
+
+That extra 1 dimension is because PyTorch assumes
+everything is in batches - we’re just using a batch size of 1 here.
+"""
+
+# Find letter index from all_letters, e.g. "a" = 0
+def letter_to_index(letter):
+    return ALL_LETTERS.find(letter)
+
+# Just for demonstrations, turn a letter into a <1 x n_letters> Tensor
+def letter_to_tensor(letter):
+    tensor = torch.zeros(1, N_LETTERS)
+    tensor[0][letter_to_index(letter)] = 1
+    return tensor
+
+# Turn a line into a <line_length x 1 n_letters>,
+# or an array of one-hot letter vectors
+
