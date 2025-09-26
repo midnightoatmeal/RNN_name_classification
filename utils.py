7353c3b466e4a50fd259c480fc87583f57270110
Lionel Rozario <lionelrozario98@gmail.com>
2025-09-26 17:12:48 -0400

    defined unicode_to_ascii

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import io
 import os
+from tkinter import ALL
 import unicodedata
 import string
 import glob
@@ -7,3 +8,14 @@ import glob
 import torch
 import random
 
+#alphabet small + capital letters + " .,;'"
+ALL_LETTERS = string.ascii_letters + " .,;'"
+N_LETTERS = len(ALL_LETTERS)
+
+# Turn a Unicode string to plain ASCII
+def unicode_to_ascii(s):
+    return ''.join(
+        c for c in unicodedata.normalize('NFD', s)
+        if unicodedata.category(c) != 'Mn'
+        and c in ALL_LETTERS
+    )
