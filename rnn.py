954d9c9c74bfb2f6f994539e5ca24a0d129f2022
Lionel Rozario <lionelrozario98@gmail.com>
2025-09-27 00:05:32 -0400

    training the model

--- rnn.py
+++ rnn.py
@@ -1,4 +1,4 @@
-from sympy import carmichael
+from sympy import carmichael, plot_implicit
 import torch
 import torch.nn as nn 
 import matplotlib.pyplot as plt 
@@ -56,4 +56,42 @@ def category_from_output(output):
     return all_categories[category_idx]
 print(category_from_output(output))
 
+
+criterion = nn.NLLLoss()
+learning_rate = 0.005
+optimizer = torch.optim.SGD(rnn.parameters(), lr=learning_rate)
+
+def train(line_tensor, category_tensor):
+    hidden = rnn.init_hidden()
+    
+    for i in range(line_tensor.size()[0]):
+        output, hidden = rnn(line_tensor[i], hidden)
+    loss = criterion(output, category_tensor)
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    return output, loss.item()
+current_loss = 0
+all_losses = []
+plot_steps, print_steps = 1000, 5000
+n_iters = 100000
+for i in range(n_iters):
+    category, line, category_tensor, line_tensor = random_training_example(category_lines, all_categories)
+
+    output, loss = train(line_tensor, category_tensor)
+    current_loss = loss
+
+    if (i+1) % plot_steps == 0:
+        all_losses.append(current_loss / plot_steps)
+        current_loss = 0
+    
+    if (i+1) % print_steps == 0:
+        guess = category_from_output(output)
+        correct = "CORRECT" if guess == category else f"WRONG {category}"
+        print(f"{i} {i/n_iters*100} {loss:.4f} {line} / {guess} {correct}")
+
+plt.figure()
+plt.plot(all_losses)
+plt.show()
 
