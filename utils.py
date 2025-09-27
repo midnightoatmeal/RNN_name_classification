e03d4867f97874e04798764edff9658a461f2099
Lionel Rozario <lionelrozario98@gmail.com>
2025-09-26 23:00:58 -0400

    defined random_training_example

--- utils.py
+++ utils.py
@@ -82,4 +82,14 @@ def random_training_example(category_lines, all_categories):
     line_tensor = line_to_tensor(line)
     return category, line, category_tensor, line_tensor
 
-    
+    if __name__ == '__main__':
+        print(ALL_LETTERS)
+        print(unicode_to_ascii('Ślusàrski'))
+
+        category_lines, all_categories = load_data()
+        print(category_lines['Italian'][:5])
+
+        print(letter_to_tensor{'3'}) # [1, 57]
+        print(line_to_tensor{'Jones'}.size()) # [5, 1, 57]
+        
+
