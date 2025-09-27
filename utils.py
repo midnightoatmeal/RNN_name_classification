bc5bcaa92538b11705c6084b430ee796ee57d112
Lionel Rozario <lionelrozario98@gmail.com>
2025-09-26 23:22:57 -0400

    cleanup

--- utils.py
+++ utils.py
@@ -7,11 +7,11 @@ import glob
 import torch
 import random
 
-#alphabet small + capital letters + " .,;'"
+# alphabet small + capital letters + " .,;'"
 ALL_LETTERS = string.ascii_letters + " .,;'"
 N_LETTERS = len(ALL_LETTERS)
 
-# Turn a Unicode string to plain ASCII
+# Turn a Unicode string to plain ASCII, thanks to https://stackoverflow.com/a/518232/2809427
 def unicode_to_ascii(s):
     return ''.join(
         c for c in unicodedata.normalize('NFD', s)
@@ -23,7 +23,7 @@ def load_data():
     # Build the category_lines dictionary, a list of names per language
     category_lines = {}
     all_categories = []
-
+    
     def find_files(path):
         return glob.glob(path)
     
@@ -31,14 +31,17 @@ def load_data():
     def read_lines(filename):
         lines = io.open(filename, encoding='utf-8').read().strip().split('\n')
         return [unicode_to_ascii(line) for line in lines]
+    
     for filename in find_files('data/names/*.txt'):
         category = os.path.splitext(os.path.basename(filename))[0]
         all_categories.append(category)
-
+        
         lines = read_lines(filename)
         category_lines[category] = lines
-
+        
     return category_lines, all_categories
+
+
 
 """
 To represent a single letter, we use a “one-hot vector” of 
@@ -56,13 +59,13 @@ everything is in batches - we’re just using a batch size of 1 here.
 def letter_to_index(letter):
     return ALL_LETTERS.find(letter)
 
-# Just for demonstrations, turn a letter into a <1 x n_letters> Tensor
+# Just for demonstration, turn a letter into a <1 x n_letters> Tensor
 def letter_to_tensor(letter):
     tensor = torch.zeros(1, N_LETTERS)
     tensor[0][letter_to_index(letter)] = 1
     return tensor
 
-# Turn a line into a <line_length x 1 n_letters>,
+# Turn a line into a <line_length x 1 x n_letters>,
 # or an array of one-hot letter vectors
 def line_to_tensor(line):
     tensor = torch.zeros(len(line), 1, N_LETTERS)
@@ -70,8 +73,9 @@ def line_to_tensor(line):
         tensor[i][0][letter_to_index(letter)] = 1
     return tensor
 
-def random_training_example(category_lines, all_categories):
 
+def random_training_example(category_lines, all_categories):
+    
     def random_choice(a):
         random_idx = random.randint(0, len(a) - 1)
         return a[random_idx]
@@ -82,14 +86,14 @@ def random_training_example(category_lines, all_categories):
     line_tensor = line_to_tensor(line)
     return category, line, category_tensor, line_tensor
 
-    if __name__ == '__main__':
-        print(ALL_LETTERS)
-        print(unicode_to_ascii('Ślusàrski'))
 
-        category_lines, all_categories = load_data()
-        print(category_lines['Italian'][:5])
 
-        print(letter_to_tensor{'3'}) # [1, 57]
-        print(line_to_tensor{'Jones'}.size()) # [5, 1, 57]
-        
-
+if __name__ == '__main__':
+    print(ALL_LETTERS)
+    print(unicode_to_ascii('Ślusàrski'))
+    
+    category_lines, all_categories = load_data()
+    print(category_lines['Italian'][:5])
+    
+    print(letter_to_tensor('J')) # [1, 57]
+    print(line_to_tensor('Jones').size()) # [5, 1, 57]
